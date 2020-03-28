e85671fa9d9adf60b903c2b27dea55d8ceb5f6a7
unknown <fayssaloudbib@gmail.com>
2020-03-28 20:02:12 +0100

    renaming variables

--- websocket-chat-example/index.js
+++ websocket-chat-example/index.js
@@ -8,18 +8,17 @@ const app = express();
 const server = http.createServer(app);
 
 //initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
+const webSocketServer = new WebSocket.Server({ server });
 var port = process.env.PORT || 3001;
 
 app.get('/', function(req, res){ // responds to the get request
   res.sendFile(__dirname + '/index.html');
 });
 
-wss.on('connection', (ws) => {
-
-  ws.on('message', (msg) => {
+webSocketServer.on('connection', (webSocket) => {
+  webSocket.on('message', (msg) => {
       let processedMsg = msg + ' [2 - back on]';
-      ws.send(processedMsg + ' [3 - back send]'); // sends message to the frontend - 3
+      webSocket.send(processedMsg + ' [3 - back send]'); // sends event to the frontend - 3
   });
 });
 
